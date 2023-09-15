70cac594d1caa95e5533d59b58f3c6738b6a4226
Jose Felipe Duarte <jose.duarte@utp.edu.co>
2023-09-14 19:36:10 -0500

    Cambios idiomas de botones

--- app/src/main/java/com/plcoding/bluetoothchat/presentation/components/DeviceScreen.kt
+++ app/src/main/java/com/plcoding/bluetoothchat/presentation/components/DeviceScreen.kt
@@ -8,11 +8,13 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.plcoding.bluetoothchat.domain.chat.BluetoothDevice
 import com.plcoding.bluetoothchat.presentation.BluetoothUiState
+import com.plcoding.bluetoothchat.R
 
 @Composable
 fun DeviceScreen(
@@ -37,10 +39,10 @@ fun DeviceScreen(
             horizontalArrangement = Arrangement.SpaceAround
         ) {
             Button(onClick = onStartScan) {
-                Text(text = "Start scan")
+                Text(text = stringResource(id = R.string.start_scan))
             }
             Button(onClick = onStopScan) {
-                Text(text = "Stop scan")
+                Text(text = stringResource(id = R.string.stop_scan))
             }
         }
     }
@@ -58,7 +60,7 @@ fun BluetoothDeviceList(
     ) {
         item {
             Text(
-                text = "Paired Devices",
+                text = stringResource(id = R.string.paired_devices),
                 fontWeight = FontWeight.Bold,
                 fontSize = 24.sp,
                 modifier = Modifier.padding(16.dp)
@@ -76,7 +78,7 @@ fun BluetoothDeviceList(
 
         item {
             Text(
-                text = "Scanned Devices",
+                text = stringResource(id = R.string.scanned_devices),
                 fontWeight = FontWeight.Bold,
                 fontSize = 24.sp,
                 modifier = Modifier.padding(16.dp)
